a37b957e5d874003673e8dbce25c24c465827d24
Tien D <devtyger@gmail.com>
2018-04-27 16:56:25 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+// you dont need the line below anymore since Handlerbars is the view engine that will take care of it
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    pageTitle: 'Home',
+    welcomeMessage: 'Welcome to my page!'
+  });
+  // res.send({
+  //   name: 'Tien',
+  //   likes: [
+  //     'watching movies',
+  //     'coding',
+  //     'training',
+  //     'eating'
+  //   ]
+  // })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+  // res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to find page. Something went wrong'
+  })
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+// app.listen(3000);
